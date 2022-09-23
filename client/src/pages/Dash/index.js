32a7f41942e036097c6a3421c561586aee483ce0
Joseph Christianson <joechristiansonwebdev@gmail.com>
2022-09-22 21:14:23 -0500

    reformatted both assessment page and dash

--- client/src/pages/Dash/index.js
+++ client/src/pages/Dash/index.js
@@ -84,23 +84,38 @@ const {queueDays,habitDays,toDos} = data.getDash
 
     return(
         <div className="dashboard-cont">
-        <section className="dash-section">
+            <div>
             <h2>Daily Queue</h2>
+
+        <section className="dash-section">
            {queueDays[0]?<QueueDay handleQueueSkip={handleQueueSkip} handleComplete={handleQueueComplete} queueDay={queueDays[0]}/>:<h2>All Done!</h2>}
         </section>
-        <section className="dash-section">
+            </div>
+            <div>
         <h2>Tasks</h2>
+        <section className="dash-section">
         <ToDos onDash={true}  userId={userId}></ToDos>
         </section>
-        <section className="dash-section">
+            </div>
+            <div>
         <h2>Today's Habits</h2>
+
+        <section className="dash-section">
         {!habitDays.length>0?<h2>No Habit Testing Today</h2>:null}
         {habitDays.map((h,i)=>{
             return <HabitDay key={i} handleComplete={handleHabitComplete} habitDay={h}/>
         })}
 
         </section>
-        
+        </div>
+        <div>
+        <h2>Feed</h2>
+
+        <section className="dash-section">
+        Feed Goes here!
+
+        </section>
+        </div>
         </div>
     )
 }
--- client/src/pages/ManageAssessments/index.js
+++ client/src/pages/ManageAssessments/index.js
@@ -1,15 +1,16 @@
 import { useQuery,useMutation } from "@apollo/client"
 import { GET_ALL_USERS_ASSESSMENTS } from "../../utils/queries"
-import "./manageAssessments.css"
 import { useState } from "react"
 import { ADD_ASSESSMENT,DELETE_ASSESSMENT } from "../../utils/mutations"
-
+import { Modal } from "../../components/Modal"
+import "./manageAssessments.css"
 export const ManageAssessments = ({userId})=>{
     const {data,loading,error,refetch} = useQuery(GET_ALL_USERS_ASSESSMENTS,{variables:{userId}})
     const [newAssessment,setNewAssessment] = useState({name:"",metric:"boolean"})
     const [addAssessment,{data:addData,loading:addLoading,error:addError}]=useMutation(ADD_ASSESSMENT)
     const [deleteAssessment,{}] = useMutation(DELETE_ASSESSMENT)
-
+    const [modalOpen,setModalOpen] = useState(false)
+    const [newOpen,setNewOpen] = useState(false)
     const handleAddChange = (e)=>{
         const key = e.target.dataset.key;
         const value = e.target.value
@@ -29,35 +30,109 @@ export const ManageAssessments = ({userId})=>{
         refetch()
     }
 
+    const openExistingAssessment = (e)=>{
+        const assessment = data.getAllUsersAssessments[e.target.dataset.index];
+        console.log(assessment);
+        setModalOpen(assessment)
+    }
+
+    const openNewAssessment = ()=>{
+        setNewOpen(true)
+    }
+
+    const closeModal = ()=>{
+        setModalOpen(false)
+    }
+
+    const graphAssessment = (e)=>{
+        const assessment = data.getAllUsersAssessments[e.target.dataset.index];
+        console.log(assessment);
+    }
+
+    const selected = ()=>{
+        return data.getAllUsersAssessments[modalOpen]
+    }
+
     if(loading){
         return(<h1>Loading</h1>)
     }
     
     return(
-        <main className="main-section">
-            <h4>
+        <>
+        <main className="main-section assessments">
+            <section className="assessment-left">
+                <div className="list">
+                    <button onClick={openNewAssessment}>Add Assessment</button>
+                    {data.getAllUsersAssessments.map((a,i)=>{
+                        return (<div key={i} className="assessment-item" ><span  onClick={graphAssessment} data-index={i}>
+                            {a.name}
+                            </span>
+                            <i onClick={openExistingAssessment} data-index={i}>e</i>
+                            </div>)
+                    })}
+                </div>
+            </section>
+            <section className="assessment-right">
+
+            </section>
+
+
+            {/* <h4>
             Manage Assessments
             </h4>
             <section className="add-assessment">
-                <input onChange={handleAddChange} data-key="name" placeholder="Assessment Name"></input>
-                <select onChange={handleAddChange} data-key="metric">
-                    <option value="boolean">Pass/Fail</option>
-                    <option value="grade">Grade</option>
-                    <option value="quantity">Quantity</option>
-                </select>
-                <button onClick={handleAddSubmit}>Add Assessment</button>
+            <input onChange={handleAddChange} data-key="name" placeholder="Assessment Name"></input>
+            <select onChange={handleAddChange} data-key="metric">
+            <option value="boolean">Pass/Fail</option>
+            <option value="grade">Grade</option>
+            <option value="quantity">Quantity</option>
+            </select>
+            <button onClick={handleAddSubmit}>Add Assessment</button>
             </section>
             <section className="assessment-grid">
-                {data.getAllUsersAssessments.map(a=>{
+            {data.getAllUsersAssessments.map(a=>{
                     return(<>
-                    <h3>{a.name}</h3>
-                    <h3>{a.metric}</h3>
-                    <button data-_id={a._id} onClick={handleDelete}>Delete</button>
-                    </>
-                    )
-                })}
-            </section>
+                        <h3>{a.name}</h3>
+                        <h3>{a.metric}</h3>
+                        <button data-_id={a._id} onClick={handleDelete}>Delete</button>
+                        </>
+                        )
+                    })}
+                </section> */}
             </main>
+            {
+            modalOpen===false?null:
+
+                <Modal
+                userId={userId}
+                modalInput={modalOpen}
+                onDash={true}
+                setModalOpen={setModalOpen}
+                >
+                    <h3>{modalOpen.metric}</h3>
+                </Modal>
+            }
+                        {
+            newOpen===false?null:
+
+                <Modal
+                userId={userId}
+                modalInput={modalOpen}
+                onDash={true}
+                setModalOpen={setNewOpen}
+                >
+                            <section className="add-assessment">
+            <input onChange={handleAddChange} data-key="name" placeholder="Assessment Name"></input>
+            <select onChange={handleAddChange} data-key="metric">
+            <option value="boolean">Pass/Fail</option>
+            <option value="grade">Grade</option>
+            <option value="quantity">Quantity</option>
+            </select>
+            <button onClick={handleAddSubmit}>Add Assessment</button>
+            </section>
+                </Modal>
+            }
+                </>
         
     )
 }
